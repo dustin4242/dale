b2420558a811727ad9ac2e6085a120ef9dd0adad
dustin4242 <thegameexposer@gmail.com>
2023-02-20 14:39:28 -0600

    Removed some unnessary lines and put the writing to screen into its own function

--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,8 @@ fn main() -> Result<(), Error> {
     .collect();
 
     let mut term = Term::stdout();
-    let mut line = 0;
-    let mut pos = file[0].len();
-    let mut screen: Screen;
-    screen = Screen {
+    let (mut line, mut pos) = (0, file[0].len());
+    let mut screen = Screen {
         line_top: 0,
         line_bottom: if file.len() < term.size().0 as usize {
             file.len()
@@ -39,80 +37,70 @@ fn main() -> Result<(), Error> {
             term.size().0 as usize
         },
     };
-    term.clear_screen()?;
-    term.write_all(
-        file[screen.line_top..screen.line_bottom]
-            .join("\n")
-            .as_bytes(),
-    )?;
-    term.move_cursor_to(pos, line)?;
+    write_screen((&mut term, &screen), (line, pos), &file)?;
     loop {
         match term.read_key() {
             Ok(console::Key::Char(x)) => {
                 file[line].insert(pos, x);
                 pos += 1;
             }
-            Ok(x) => match x {
-                console::Key::Backspace => {
-                    if line != 0 && pos == 0 {
-                        pos = file[line - 1].len();
-                        let currentline = file[line].clone();
-                        file[line - 1].push_str(currentline.as_str());
-                        file.remove(line);
-                        line -= 1;
-                    } else {
-                        file[line].remove(pos - 1);
-                        pos -= 1;
-                    }
-                }
-                console::Key::Enter => {
+            Ok(console::Key::Backspace) => {
+                if line != 0 && pos == 0 {
+                    pos = file[line - 1].len();
                     let currentline = file[line].clone();
-                    let newlines = currentline.split_at(pos);
-                    file[line] = newlines.0.to_string();
-                    file.insert(line + 1, newlines.1.to_string());
+                    file[line - 1].push_str(currentline.as_str());
+                    file.remove(line);
+                    line -= 1;
+                } else {
+                    file[line].remove(pos - 1);
+                    pos -= 1;
+                }
+            }
+            Ok(console::Key::Enter) => {
+                let currentline = file[line].clone();
+                let newlines = currentline.split_at(pos);
+                file[line] = newlines.0.to_string();
+                file.insert(line + 1, newlines.1.to_string());
+                line += 1;
+                pos = 0;
+            }
+            Ok(console::Key::Tab) => {
+                file[line].push_str("    ");
+                pos += 4;
+            }
+            Ok(console::Key::ArrowUp) => {
+                if line != 0 {
+                    line -= 1;
+                    if file[line].len() < pos {
+                        pos = file[line].len();
+                    }
+                }
+            }
+            Ok(console::Key::ArrowDown) => {
+                if line + 1 < file.len() {
                     line += 1;
-                    pos = 0;
-                }
-                console::Key::Tab => {
-                    file[line].push_str("    ");
-                    pos += 4;
-                }
-                console::Key::ArrowUp => {
-                    if line != 0 {
-                        line -= 1;
-                        if file[line].len() < pos {
-                            pos = file[line].len();
-                        }
+                    if file[line].len() < pos {
+                        pos = file[line].len();
                     }
                 }
-                console::Key::ArrowDown => {
-                    if line + 1 < file.len() {
-                        line += 1;
-                        if file[line].len() < pos {
-                            pos = file[line].len();
-                        }
-                    }
+            }
+            Ok(console::Key::ArrowRight) => {
+                if pos != file[line].len() {
+                    pos += 1;
                 }
-                console::Key::ArrowRight => {
-                    if pos != file[line].len() {
-                        pos += 1;
-                    }
+            }
+            Ok(console::Key::ArrowLeft) => {
+                if pos != 0 {
+                    pos -= 1;
                 }
-                console::Key::ArrowLeft => {
-                    if pos != 0 {
-                        pos -= 1;
-                    }
-                }
-                console::Key::Escape => {
-                    fs::write(file_path, file.join("\n"))
-                        .expect("Was Unable To Save File Contents");
-                    exit(0)
-                }
-                _ => (),
-            },
+            }
+            Ok(console::Key::Escape) => {
+                fs::write(file_path, file.join("\n")).expect("Was Unable To Save File Contents");
+                exit(0)
+            }
             Err(x) => panic!("{}", x),
+            _ => (),
         }
-        term.clear_screen()?;
         if term.size().0 as usize >= file.len() {
             screen.line_top = 0;
             screen.line_bottom = file.len();
@@ -128,11 +116,22 @@ fn main() -> Result<(), Error> {
             }
             (false, false) => (),
         };
-        term.write_all(
-            file[screen.line_top..screen.line_bottom]
-                .join("\n")
-                .as_bytes(),
-        )?;
-        term.move_cursor_to(pos, line - screen.line_top)?;
+        write_screen((&mut term, &screen), (line, pos), &file)?;
     }
+}
+
+// Seperated Functions
+fn write_screen(
+    (term, screen): (&mut Term, &Screen),
+    (line, pos): (usize, usize),
+    file: &Vec<String>,
+) -> Result<(), Error> {
+    term.clear_screen()?;
+    term.write_all(
+        file[screen.line_top..screen.line_bottom]
+            .join("\n")
+            .as_bytes(),
+    )?;
+    term.move_cursor_to(pos, line - screen.line_top)?;
+    Ok(())
 }
