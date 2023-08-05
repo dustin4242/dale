2861c87be2f2d9c751dde9228c6f7b927f550ff2
dustin4242 <thegameexposer@gmail.com>
2023-08-05 04:05:37 -0500

    Put Screen struct into its own file along with making the seperated functions relative to the screen. Also made the code for moving the screen up and down more intuitive by moving it from the loop into the up and down arrow code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
 use console::Term;
-use std::{
-    env, fs,
-    io::{Error, Write},
-    process::exit,
-};
+use std::{env, fs, io::Error};
 
-struct Screen {
-    line: usize,
-    pos: usize,
-    line_top: usize,
-    line_bottom: usize,
-    info_line: String,
-}
+mod screen;
+use screen::Screen;
 
 fn main() -> Result<(), Error> {
     let mut temp_path = env::args().nth(1).expect("Didn't Supply A File To Edit");
     let isrootdir = temp_path.starts_with("/");
     let ishomedir = temp_path.starts_with("~");
-    println!("{}", temp_path);
     let file_path = if isrootdir {
         temp_path
     } else if ishomedir {
@@ -37,141 +27,25 @@ fn main() -> Result<(), Error> {
         .map(|x| x.to_string())
         .collect();
     file.pop();
+
     let mut term = Term::stdout();
-    let mut screen = Screen {
-        line: 0,
-        pos: file[0].len(),
-        line_top: 0,
-        line_bottom: if file.len() < term.size().0 as usize {
-            file.len()
-        } else {
-            term.size().0 as usize - 1
+    let mut screen = Screen::new(
+        0,
+        0,
+        0,
+        match file.len() < term.size().0 as usize - 1 {
+            true => file.len(),
+            false => term.size().0 as usize - 1,
         },
-        info_line: file_path.to_owned(),
-    };
-    write_screen(&mut term, &screen, &file);
+        file_path.to_owned(),
+    );
+    screen.write_term(&mut term, &file);
     loop {
         if term.size().0 as usize >= file.len() {
             screen.line_top = 0;
             screen.line_bottom = file.len();
         }
-        match (
-            screen.line_top > screen.line,
-            screen.line_bottom <= screen.line,
-        ) {
-            (true, _) => {
-                screen.line_top -= 1;
-                screen.line_bottom -= 1;
-            }
-            (_, true) => {
-                screen.line_top += 1;
-                screen.line_bottom += 1;
-            }
-            (false, false) => (),
-        };
-        write_screen(&mut term, &screen, &file);
-        handle_key(&mut term, &mut screen, &mut file, &file_path);
-    }
-}
-
-// Seperated Functions
-fn add_char(screen: &mut Screen, file: &mut Vec<String>, char: char) {
-    file[screen.line].insert(screen.pos, char);
-    screen.pos += 1;
-}
-
-fn remove_char(screen: &mut Screen, file: &mut Vec<String>) {
-    if screen.line != 0 && screen.pos == 0 {
-        let current_line = file[screen.line].clone();
-        screen.pos = file[screen.line - 1].len();
-        file[screen.line - 1].push_str(current_line.as_str());
-        file.remove(screen.line);
-        screen.line -= 1;
-    } else {
-        file[screen.line].remove(screen.pos - 1);
-        screen.pos -= 1;
-    }
-}
-
-fn create_newline(screen: &mut Screen, file: &mut Vec<String>) {
-    let current_line = file[screen.line].clone();
-    let new_lines = current_line.split_at(screen.pos);
-    file[screen.line] = new_lines.0.to_string();
-    file.insert(screen.line + 1, new_lines.1.to_string());
-    screen.line += 1;
-    screen.pos = 0;
-}
-
-fn write_screen(term: &mut Term, screen: &Screen, file: &Vec<String>) {
-    let size = term.size();
-    term.clear_screen().unwrap();
-    term.write_all(
-        format!("\n{}", file[screen.line_top..screen.line_bottom].join("\n")).as_bytes(),
-    )
-    .unwrap();
-    let rest_of_screen = (size.1 as usize)
-        .checked_sub(screen.info_line.len())
-        .unwrap();
-    term.move_cursor_to(0, (size.0 - 1) as usize).unwrap();
-    term.write_all(
-        format!(
-            "\x1b[41m\x1b[30m\n{}{}\x1b[37m\x1b[40m",
-            screen.info_line,
-            " ".repeat(rest_of_screen)
-        )
-        .as_bytes(),
-    )
-    .unwrap();
-    term.move_cursor_to(screen.pos, screen.line - screen.line_top)
-        .unwrap();
-}
-
-fn handle_key(term: &mut Term, screen: &mut Screen, file: &mut Vec<String>, file_path: &String) {
-    match term.read_key().unwrap() {
-        console::Key::UnknownEscSeq(x) => match x[0].to_string().as_str() {
-            "s" => match fs::write(file_path, file.join("\n")) {
-                Err(_) => screen.info_line = "Unable To Save Contents".to_owned(),
-                Ok(_) => screen.info_line = "Saved Contents".to_owned(),
-            },
-            x => panic!("{x}"),
-        },
-        console::Key::Char(x) => add_char(screen, file, x),
-        console::Key::Backspace => remove_char(screen, file),
-        console::Key::Enter => create_newline(screen, file),
-        console::Key::Tab => {
-            file[screen.line].push_str("    ");
-            screen.pos += 4;
-        }
-        console::Key::ArrowUp => {
-            if screen.line != 0 {
-                screen.line -= 1;
-                if file[screen.line].len() < screen.pos {
-                    screen.pos = file[screen.line].len();
-                }
-            }
-        }
-        console::Key::ArrowDown => {
-            if screen.line + 1 < file.len() {
-                screen.line += 1;
-                if file[screen.line].len() < screen.pos {
-                    screen.pos = file[screen.line].len();
-                }
-            }
-        }
-        console::Key::ArrowRight => {
-            if screen.pos != file[screen.line].len() {
-                screen.pos += 1;
-            }
-        }
-        console::Key::ArrowLeft => {
-            if screen.pos != 0 {
-                screen.pos -= 1;
-            }
-        }
-        console::Key::Escape => {
-            term.clear_screen().unwrap();
-            exit(0);
-        }
-        _ => (),
+        screen.write_term(&mut term, &file);
+        screen.handle_key(&mut term, &mut file, &file_path);
     }
 }
---
+++ src/screen.rs
@@ -0,0 +1,130 @@
+use console::Term;
+use std::{fs, io::Write, process::exit};
+
+pub struct Screen {
+    pub line: usize,
+    pub pos: usize,
+    pub line_top: usize,
+    pub line_bottom: usize,
+    pub info_line: String,
+}
+
+impl Screen {
+    pub fn new(
+        line: usize,
+        pos: usize,
+        line_top: usize,
+        line_bottom: usize,
+        info_line: String,
+    ) -> Screen {
+        Screen {
+            line,
+            pos,
+            line_top,
+            line_bottom,
+            info_line,
+        }
+    }
+    pub fn add_char(&mut self, file: &mut Vec<String>, char: char) {
+        file[self.line].insert(self.pos, char);
+        self.pos += 1;
+    }
+    pub fn remove_char(&mut self, file: &mut Vec<String>) {
+        if self.line != 0 && self.pos == 0 {
+            let current_line = file[self.line].clone();
+            self.pos = file[self.line - 1].len();
+            file[self.line - 1].push_str(current_line.as_str());
+            file.remove(self.line);
+            self.line -= 1;
+        } else {
+            file[self.line].remove(self.pos - 1);
+            self.pos -= 1;
+        }
+    }
+    pub fn newline(&mut self, file: &mut Vec<String>) {
+        let current_line = file[self.line].clone();
+        let new_lines = current_line.split_at(self.pos);
+        file[self.line] = new_lines.0.to_string();
+        file.insert(self.line + 1, new_lines.1.to_string());
+        self.line += 1;
+        self.pos = 0;
+    }
+    pub fn write_term(&mut self, term: &mut Term, file: &Vec<String>) {
+        let size = term.size();
+        term.clear_screen().unwrap();
+        term.write_all(
+            format!("\n{}", file[self.line_top..self.line_bottom].join("\n")).as_bytes(),
+        )
+        .unwrap();
+        let rest_of_screen = (size.1 as usize).checked_sub(self.info_line.len()).unwrap();
+        term.move_cursor_to(0, (size.0 - 1) as usize).unwrap();
+        term.write_all(
+            format!(
+                "\x1b[41m\x1b[30m\n{}{}\x1b[37m\x1b[40m",
+                self.info_line,
+                " ".repeat(rest_of_screen)
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+        term.move_cursor_to(self.pos, self.line - self.line_top)
+            .unwrap();
+    }
+    pub fn handle_key(&mut self, term: &mut Term, file: &mut Vec<String>, file_path: &String) {
+        match term.read_key().unwrap() {
+            console::Key::UnknownEscSeq(x) => match x[0].to_string().as_str() {
+                "s" => match fs::write(file_path, file.join("\n")) {
+                    Err(_) => self.info_line = "Unable To Save Contents".to_owned(),
+                    Ok(_) => self.info_line = "Saved Contents".to_owned(),
+                },
+                x => panic!("{x}"),
+            },
+            console::Key::Char(x) => self.add_char(file, x),
+            console::Key::Backspace => self.remove_char(file),
+            console::Key::Enter => self.newline(file),
+            console::Key::Tab => {
+                file[self.line].push_str("    ");
+                self.pos += 4;
+            }
+            console::Key::ArrowUp => {
+                if self.line != 0 {
+                    self.line -= 1;
+                    if file[self.line].len() < self.pos {
+                        self.pos = file[self.line].len();
+                    }
+                    if self.line_top > self.line {
+                        self.line_top -= 1;
+                        self.line_bottom -= 1;
+                    }
+                }
+            }
+            console::Key::ArrowDown => {
+                if self.line + 1 < file.len() {
+                    self.line += 1;
+                    if file[self.line].len() < self.pos {
+                        self.pos = file[self.line].len();
+                    }
+                    if self.line_bottom - 1 < self.line {
+                        self.line_top += 1;
+                        self.line_bottom += 1;
+                    }
+                }
+            }
+            console::Key::ArrowRight => {
+                if self.pos != file[self.line].len() {
+                    self.pos += 1;
+                }
+            }
+            console::Key::ArrowLeft => {
+                if self.pos != 0 {
+                    self.pos -= 1;
+                }
+            }
+            console::Key::Escape => {
+                term.clear_screen().unwrap();
+                exit(0);
+            }
+            _ => (),
+        }
+    }
+}
