e713fde92cad7c8fdbf261793881ea9dac4f3eb8
dustin4242 <thegameexposer@gmail.com>
2023-08-15 01:22:10 -0500

    Fixing a few possible errors if no tables are supplied in the plugins

--- src/syntax.rs
+++ src/syntax.rs
@@ -1,12 +1,23 @@
 use regex::Regex;
 use std::collections::HashMap;
-use toml::Value;
+use toml::{map::Map, Value};
 
 pub fn highlight(plugin: Option<toml::Value>, mut file: String) -> String {
+    let empty_table = Value::Table(Map::new());
+    let empty_array = Value::Array(Vec::new());
     let syntax = plugin.unwrap();
     let mut replace_syntax = HashMap::new();
-    let basic_table = syntax.get("basic").unwrap();
-    let keywords = basic_table.get("keywords").unwrap().as_array().unwrap();
+    let basic_table = syntax.get("basic").unwrap_or(&empty_table);
+    let keywords = basic_table
+        .get("keywords")
+        .unwrap_or(&empty_array)
+        .as_array()
+        .unwrap();
+    let types = basic_table
+        .get("types")
+        .unwrap_or(&empty_array)
+        .as_array()
+        .unwrap();
     for key in keywords {
         let replace = key.as_str().unwrap();
         replace_syntax.insert(
@@ -14,7 +25,6 @@ pub fn highlight(plugin: Option<toml::Value>, mut file: String) -> String {
             format!("\x1b[32m{replace}\x1b[37m"),
         );
     }
-    let types = basic_table.get("types").unwrap().as_array().unwrap();
     for key_type in types {
         let replace = key_type.as_str().unwrap();
         replace_syntax.insert(
@@ -59,19 +69,19 @@ fn basic_op<T: ToString>(
 ) {
     let option = table
         .get(option_name.to_string())
-        .unwrap()
+        .unwrap_or(&Value::Boolean(false))
         .as_bool()
         .unwrap();
-    match color {
-        "red" => color = "\x1b[31m",
-        "green" => color = "\x1b[32m",
-        "yellow" => color = "\x1b[33m",
-        "blue" => color = "\x1b[34m",
-        "magenta" => color = "\x1b[35m",
-        "cyan" => color = "\x1b[36m",
-        x => panic!("Unknown Color In Syntax: {}", x),
-    }
     if option {
+        match color {
+            "red" => color = "\x1b[31m",
+            "green" => color = "\x1b[32m",
+            "yellow" => color = "\x1b[33m",
+            "blue" => color = "\x1b[34m",
+            "magenta" => color = "\x1b[35m",
+            "cyan" => color = "\x1b[36m",
+            x => panic!("Unknown Color In Syntax: {}", x),
+        }
         let option_regex = Regex::new(regex).unwrap();
         let temp_file = file.clone();
         let mut finds = option_regex.find_iter(&temp_file);
@@ -89,10 +99,15 @@ fn basic_op<T: ToString>(
     }
 }
 fn custom_op(syntax: &Value, file: &mut String) {
-    let custom_table = syntax.get("custom").unwrap().as_table().unwrap();
+    let empty_table = Value::Table(Map::new());
+    let custom_table = syntax
+        .get("custom")
+        .unwrap_or(&empty_table)
+        .as_table()
+        .unwrap();
     for syntax in custom_table {
         let custom_regex = Regex::new(syntax.0).unwrap();
-        let mut color = syntax.1.as_str().unwrap();
+        let mut color = syntax.1.as_str().unwrap_or("white");
         match color {
             "red" => color = "\x1b[31m",
             "green" => color = "\x1b[32m",
@@ -100,6 +115,7 @@ fn custom_op(syntax: &Value, file: &mut String) {
             "blue" => color = "\x1b[34m",
             "magenta" => color = "\x1b[35m",
             "cyan" => color = "\x1b[36m",
+            "white" => color = "\x1b[37m",
             x => panic!("Unknown Color In Syntax: {}", x),
         }
         let temp_file = file.clone();
