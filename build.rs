51672eb23ad972b7cc91d6d7f2f11bb63bf6de01
dustin4242 <thegameexposer@gmail.com>
2023-09-04 22:12:44 -0500

    Only need one license in cargo toml and made the build a little more error friendly

--- build.rs
+++ build.rs
@@ -1,15 +1,21 @@
-use std::{env, process::Command};
+use std::{env, fs::create_dir, process::Command};
 
 fn main() {
-    let home = env::var("HOME").unwrap();
-    let cwd = env::current_dir().unwrap();
-    println!("cargo:rerun-if-changed={home}/.config/dale/plugins");
-    Command::new("cp")
-        .args([
-            "-r",
-            format!("{}/plugins", cwd.to_str().unwrap()).as_str(),
-            format!("{home}/.config/dale").as_str(),
-        ])
-        .spawn()
-        .unwrap();
+    let home = env::var("HOME");
+    let cwd = env::current_dir();
+    match (home, cwd) {
+        (Ok(home), Ok(cwd)) => {
+            create_dir(format!("{home}/.config/dale")).unwrap_or_default();
+            println!("cargo:rerun-if-changed={home}/.config/dale/plugins");
+            Command::new("cp")
+                .args([
+                    "-r",
+                    format!("{}/plugins", cwd.to_str().unwrap()).as_str(),
+                    format!("{home}/.config/dale").as_str(),
+                ])
+                .spawn()
+                .unwrap();
+        }
+        _ => (),
+    }
 }
