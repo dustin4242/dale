02113335f6bd708e8b6d0809681c94f553f34bde
dustin4242 <thegameexposer@gmail.com>
2023-09-04 21:56:39 -0500

    Added a build script to make configuration easier

---
+++ build.rs
@@ -0,0 +1,16 @@
+use std::{env, process::Command};
+
+fn main() {
+    let home = env::var("HOME").unwrap();
+    let cwd = env::current_dir().unwrap();
+    println!("cargo:rerun-if-changed={}/.config/dale/plugins", home);
+    println!("here");
+    Command::new("cp")
+        .args([
+            "-r",
+            format!("{}/plugins", cwd.to_str().unwrap()).as_str(),
+            format!("{home}/.config/dale").as_str(),
+        ])
+        .spawn()
+        .unwrap();
+}
